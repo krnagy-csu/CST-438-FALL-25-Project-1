c0a55dd7c543d7eb4a009ebea5beca345aa10cf6
Lily <rlily13saavedra@gmail.com>
2025-09-04 12:57:29 -0700

    Made more changes to the home page. Added a button for searching. No functionalities yet.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,25 @@
+// Recources: https://reactnative.dev/docs/handling-touches
+
+
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 import { Text, View } from 'react-native';
+import{Alert, Button} from 'react-native';
 
 
-import { HelloWave } from '@/components/HelloWave';
+// import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { SearchBar } from 'react-native-screens';
+// import { Button } from '@react-navigation/elements';
+
+
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#94d0f1ff', dark: '#117572ff' }}
+      headerBackgroundColor={{ light: '#bee8ffff', dark: '#437996ff' }}
 
       headerImage={
         <Image
@@ -21,25 +29,39 @@ export default function HomeScreen() {
       }>
 
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Discover countless titles</ThemedText>
+        {/* <HelloWave /> */}
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
+        <ThemedText type="subtitle">Browse through our library</ThemedText>
+        {/* <ThemedText> */}
+          {/* Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '} */}
+          {/* <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
               web: 'F12',
             })}
           </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+          to open developer tools. */}
+          
+        {/* </ThemedText> */}
+        <SearchBar
+          placeholder = "Search"
+        // onChangeText={this.updateSearch}
+        // value={search}
+          />
+          <Button
+            onPress={() => {
+              console.log('tapped')
+            }}
+            title="Enter"
+          />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+      
+      {/* _________________________________________________________ */}
+      {/* <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
           {`Tap the Explore tab to learn more about what's included in this starter app.`}
@@ -54,7 +76,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
-      </ThemedView>
+      </ThemedView> */}
     </ParallaxScrollView>
   );
 }
@@ -76,4 +98,10 @@ const styles = StyleSheet.create({
     left: -80,
     position: 'absolute',
   },
+  buttonContainer:{
+    margin:20,
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection:'row'
+  }
 });
