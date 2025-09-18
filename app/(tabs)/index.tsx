61583e5b4249af0e5604de8f460185caa97dbdb8
gabe-tommi <gabetgal2112@gmail.com>
2025-09-18 13:28:21 -0700

    re added old state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 import{Alert, Button} from 'react-native';
 //for search bar
 import {TextInput, SafeAreaView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 
 
@@ -15,11 +16,33 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import AuthCheck from '@/components/registrationComponents/authCheck';
+import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { router } from 'expo-router';
+
+useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const userToken = await AsyncStorage.getItem('userToken');
+        if (userToken !== 'loggedIn') {
+          router.replace('/login');
+        }
+      } catch (error) {
+        console.error('Error checking authentication:', error);
+        router.replace('/login');
+      }
+    };
+
+    checkAuth();
+  }, []);
+
 
 const textEx = () => {
   const[text, onChangeText] = React.useState('Ehhhh');
   const [number, onChangeNumber] = React.useState('');
 }
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
@@ -100,7 +123,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText> */}
       </ThemedView>
-    </ParallaxScrollView>
+    </ParallaxScrollView>    
   );
 }
 
